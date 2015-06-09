3ca856c5848e112528f3e6625ade23d6f2a18da5
Mitchell <mitchell@vitez.me>
2015-06-09 11:51:17 -0400

    Added unicode alphabet and fixed character conversion functions

--- lookalike.py
+++ lookalike.py
@@ -6,15 +6,25 @@ Does steganography on a message by using similar-looking unicode characters
 """
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-# TODO: Fill in the rest of the unicode alphabet
-unicodeAlphabet = [u'\u212B', u'\u212C', u'\u2102']
-END_OF_ASCII = 256
+unicodeAlphabet = [u'\u0251', u'\u0185', u'\u010B', u'\u0257', u'\u0259',
+				   u'\u0192', u'\u01E5', u'\u0127', u'\u0269', u'\u0237',
+				   u'\u0138', u'\u026D', u'\u0271', u'\u0273', u'\u01A1',
+				   u'\u01A5', u'\u01A3', u'\u0157', u'\u01A7', u'\u0165',
+				   u'\u01DA', u'\u03BD', u'\u03E3', u'\u0436', u'\u045E',
+				   u'\u0291']
+END_OF_ASCII = 128
 
 def unicodeToAscii(character):
-	return alphabet[unicodeAlphabet.index(character)]
+	if character in unicodeAlphabet:
+		return alphabet[unicodeAlphabet.index(character)]
+	else:
+		return ''
 
 def asciiToUnicode(character):
-	return unicodeAlphabet[alphabet.index(character)]
+	if character in alphabet:
+		return unicodeAlphabet[alphabet.index(character)]
+	else:
+		return ''
 
 def convertString(message, steg):
 	message = message.lower()
