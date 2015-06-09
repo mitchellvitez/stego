23cc4ea3918f5767dda94eb1cc11b9fc76204297
Mitchell Vitez <mitchellvitez@Mitchells-MacBook-Pro.local>
2015-06-09 14:41:45 -0400

    Added encode and decode functions to ave maria cipher

--- avemaria.py
+++ avemaria.py
@@ -1,3 +1,13 @@
+# Mitchell Vitez, 6/9/15
+
+"""
+Ave Maria Cipher - Converts messages to Latin exultations
+"""
+
+import random
+
+PERCENT_NOUNS = .30 # * 100 to get actual percentage
+
 nouns = { "a":"Deus",
 		"b":"Creator",
 		"c":"Conditor",
@@ -22,7 +32,7 @@ nouns = { "a":"Deus",
 		"y":"Pastor",
 		"z":"Moderator" }
 
-reverse_nouns = dict(reversed(item) for item in forward_map.items())
+reverse_nouns = dict(reversed(item) for item in nouns.items())
 
 adjectives = { "a":"clemens",
 		"b":"clementibimus",
@@ -48,4 +58,34 @@ adjectives = { "a":"clemens",
 		"y":"cunctipotens",
 		"z":"magnificus" }
 
-reverse_adjectives = dict(reversed(item) for item in forward_map.items())
+reverse_adjectives = dict(reversed(item) for item in adjectives.items())
+
+def decodePrayer(message):
+	letters = []
+	for word in message.split(' '):
+		if word.endswith(','):
+			word = word[:-1]
+		letters.append(reverse_nouns.get(word, ''))
+		letters.append(reverse_adjectives.get(word, ''))
+	return ''.join(letters)
+
+def encodePrayer(message):
+	words = []
+	isNoun = True
+	for letter in message:
+		letter = letter.lower()
+		if not letter.isalpha():
+			continue
+		if isNoun:
+			words.append(nouns[letter])
+		else:
+			words.append(adjectives[letter])
+			words.append(',')
+		words.append(' ')
+		isNoun = random.random() < PERCENT_NOUNS
+	return ''.join(words)
+
+if __name__ == '__main__':
+	import fileinput
+	for message in fileinput.input():
+		print encodePrayer(message)
